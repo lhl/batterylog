aaf915cc6c4f8756e9c7c2023a5e692c666190b5
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-08-19 18:23:06 +0545

    Copy the content of the local git repo for insallation.
    - For development purposes, it mightn't be feasible to get root access to edit the files inside of /opt/batterylog which is the folder of installation to which every file got moved prior to this. So, instead of moving the whole repository keeping a local copy as to make it easier for development.
    
    - Also adding log messages for installation as to debug installation errors (if any).
    
    - Adding .sleep to the extension of batterylog script used pior to suspend
    and after resuming to keep it consistent with other files in
    /usr/lib/systemd/system-sleep/.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+install_dir="/opt/batterylog"
+suspend_resume_script_location="/usr/lib/systemd/system-sleep/batterylog.sleep"  # systemd's location for script running on suspend/resume
+
 echo 'This app requires sqlite3 and python3 to run'
 
 # Create Local database
-sqlite3 batterylog.db < schema.sql
+sqlite3 batterylog.db < schema.sql && echo "Successfully created local database."
 
 # Run logging on suspend and resume
-sudo cp batterylog.system-sleep /usr/lib/systemd/system-sleep/batterylog
+sudo cp batterylog.system-sleep "$suspend_resume_script_location" && echo "Added logging on suspend and resume"
 
 # Install
-sudo mkdir -p /opt
-sudo mv ../batterylog /opt/batterylog
-
+[ ! -d "/opt" ] && sudo mkdir -p /opt && echo "Created directory /opt as it didn't exist."
+sudo cp -r ../batterylog "$install_dir" && echo "Successfully install batterylog in $install_dir."
